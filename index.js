dc35405a254a2db3df4ec650063d5ab6f60242e3
Axel Sandoval <axesan917@gmail.com>
2024-10-26 14:20:38 -0600

    uploads configured

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 let express = require('express')
 
+let multer = require('multer')
+
+let xlsx = require('xlsx')
+
+const fs = require('fs')
+
+const path = require('path')
+
+const upload = multer({
+    dest: "uploads/"
+})
+
 let app = express()
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
     res.render('index')
+})
+
+app.post('/exceltojson',upload.single('file'), (req,res)=>{
+    console.log(req.file.path)
 })
 
 app.listen(5000,() =>{
