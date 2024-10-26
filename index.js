ecfed631c032a9975c23bf30e8b45c20d45cdefd
Axel Sandoval <axesan917@gmail.com>
2024-10-26 14:30:01 -0600

    Data extraction complete

--- index.js
+++ index.js
@@ -22,6 +22,44 @@ app.get('/', (req, res)=>{
 
 app.post('/exceltojson',upload.single('file'), (req,res)=>{
     console.log(req.file.path)
+    try{
+
+        const workbook = xlsx.readFile(req.file.path)
+
+        const sheetNames = workbook.SheetNames
+
+        const allData = sheetNames.reduce((acc, sheetName)=>{
+            
+            const worksheet = workbook.Sheets[sheetName]
+
+            const jsonData = xlsx.utils.sheet_to_json(worksheet)
+
+            acc[sheetName] = jsonData
+
+            return acc
+
+        },{})
+
+        const jsonCointent = JSON.stringify(allData, null, 2)
+
+        const outputPath = path.join(__dirname, "uploads", "output.json")
+
+        fs.writeFileSync(outputPath, jsonCointent, "utf-8")
+
+        // Download the file 
+
+        res.download(outputPath, "output.son", (err) => {
+            if(err){
+                console.log(err)
+            }else{
+                fs.unlinkSync(req.file.path)
+                fs.unlinkSync(outputPath)
+            }
+        })
+
+    }catch(error){
+        console.log(error)
+    }
 })
 
 app.listen(5000,() =>{
